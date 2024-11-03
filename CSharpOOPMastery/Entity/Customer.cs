a72dbb4078eece28f686f0e4138239cb5f2af8d7
mustafamelihcetin <mustafamelihcetin@outlook.com>
2024-11-03 23:02:49 +0300

    Adding entity classes.

---
+++ CSharpOOPMastery/Entity/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace CSharpOOPMastery.Entity
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+    }
+}
